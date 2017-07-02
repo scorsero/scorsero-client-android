e016e18a05315f3fef81d8ec516091a499c44f19
dmi3coder <dmi3coder@gmail.com>
2017-07-02 19:10:32 +0300

    added layout to MainController raw

--- app/src/main/java/io/github/dmi3coder/scorsero/main/MainController.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/main/MainController.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
+import io.github.dmi3coder.scorsero.R
 import io.github.dmi3coder.scorsero.main.MainContract.Presenter
 
 /**
@@ -12,7 +13,9 @@ import io.github.dmi3coder.scorsero.main.MainContract.Presenter
 class MainController : Controller(), MainContract.View {
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
-    TODO("not implemented")
+    val view = inflater.inflate(R.layout.controller_main, container, false)
+
+    return view
   }
 
   override fun setPresenter(presenter: Presenter) {

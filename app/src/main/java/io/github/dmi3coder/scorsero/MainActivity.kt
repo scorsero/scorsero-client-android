b63dbf768247bd15c2dc5ffbd5f5c857ad5baaca
dmi3coder <dmi3coder@gmail.com>
2017-07-03 18:13:19 +0300

    MainActivity moved to AppCompat MainActivity's core container finalized

--- app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
@@ -1,20 +1,20 @@
 package io.github.dmi3coder.scorsero
 
-import android.arch.lifecycle.LifecycleActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import android.widget.FrameLayout
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
 import io.github.dmi3coder.scorsero.main.MainController
 
-class MainActivity : LifecycleActivity() {
+class MainActivity : AppCompatActivity() {
 
   var router: Router? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    var container = FrameLayout(this)
+    val container = FrameLayout(this)
     setContentView(container)
     router = Conductor.attachRouter(this, container, savedInstanceState)
     if (!router!!.hasRootController()) {

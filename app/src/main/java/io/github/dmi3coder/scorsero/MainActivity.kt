214ee17f7e6e615c915674d35327abcda5b9029e
dmi3coder <dmi3coder@gmail.com>
2017-07-03 12:38:30 +0300

    added main router raw MainController connected to MainActivity raw

--- app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
@@ -2,11 +2,29 @@ package io.github.dmi3coder.scorsero
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.FrameLayout
+import com.bluelinelabs.conductor.Conductor
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
+import io.github.dmi3coder.scorsero.main.MainController
 
 class MainActivity : AppCompatActivity() {
 
+  var router: Router? = null
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
+    var container = FrameLayout(this)
+    setContentView(container)
+    router = Conductor.attachRouter(this, container, savedInstanceState)
+    if (!router!!.hasRootController()) {
+      router!!.setRoot(RouterTransaction.with(MainController()))
+    }
+  }
+
+  override fun onBackPressed() {
+    if (!router!!.handleBack()) {
+      super.onBackPressed()
+    }
   }
 }

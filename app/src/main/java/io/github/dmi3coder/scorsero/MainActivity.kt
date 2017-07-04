2bf8b24258fe8600d95f0396110dcc12bf3af7d2
dmi3coder <dmi3coder@gmail.com>
2017-07-04 18:30:11 +0300

    empty bottomsheet w\ coordinator layout

--- app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
@@ -1,23 +1,30 @@
 package io.github.dmi3coder.scorsero
 
 import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
-import android.widget.FrameLayout
+import android.view.View
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
 import io.github.dmi3coder.scorsero.main.MainController
 import io.github.dmi3coder.scorsero.main.MainPresenter
+import kotlinx.android.synthetic.main.activity_main.bottom_sheet_frame
+import kotlinx.android.synthetic.main.activity_main.main_frame
 
 class MainActivity : AppCompatActivity() {
 
   var router: Router? = null
+  var bottomSheetBehavior: BottomSheetBehavior<View>? = null
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    val container = FrameLayout(this)
-    setContentView(container)
-    router = Conductor.attachRouter(this, container, savedInstanceState)
+    setContentView(R.layout.activity_main)
+    router = Conductor.attachRouter(this, main_frame, savedInstanceState)
+
+    bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_frame)
+    bottomSheetBehavior?.isHideable = true
+
     if (!router!!.hasRootController()) {
       val mainController = MainController()
       val mainPresenter = MainPresenter(mainController)

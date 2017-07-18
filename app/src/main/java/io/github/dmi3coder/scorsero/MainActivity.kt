f24bb1be8250dae20766a280f568834449c16421
dmi3coder <dmi3coder@gmail.com>
2017-07-18 23:18:35 +0300

    Fab & bottom sheet moved to MainController

--- app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/MainActivity.kt
@@ -1,60 +1,38 @@
 package io.github.dmi3coder.scorsero
 
 import android.os.Bundle
-import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
-import android.view.View
 import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Router
 import com.bluelinelabs.conductor.RouterTransaction
 import io.github.dmi3coder.scorsero.main.MainController
 import io.github.dmi3coder.scorsero.main.MainPresenter
-import io.github.dmi3coder.scorsero.score.ScoreStarterController
-import io.github.dmi3coder.scorsero.score.ScoreCreationPresenter
-import kotlinx.android.synthetic.main.activity_main.bottom_sheet_frame
 import kotlinx.android.synthetic.main.activity_main.main_frame
-import kotlinx.android.synthetic.main.activity_main.main_starter_fab
 import kotlinx.android.synthetic.main.activity_main.toolbar
 import org.joda.time.DateTime
 
 class MainActivity : AppCompatActivity() {
 
   var router: Router? = null
-  var bottomSheetRouter: Router? = null
-  var bottomSheetBehavior: BottomSheetBehavior<View>? = null
-
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
     setupToolbar()
     showScreen(savedInstanceState)
-    showBottomSheet(savedInstanceState)
   }
 
 
   fun showScreen(savedInstanceState: Bundle?) {
     router = Conductor.attachRouter(this, main_frame, savedInstanceState)
     if (!router!!.hasRootController()) {
-      val mainController = MainController()
+      val mainController = MainController(savedInstanceState)
       val mainPresenter = MainPresenter(mainController)
       router!!.setRoot(RouterTransaction.with(mainController))
       mainPresenter.start()
     }
   }
 
-  fun showBottomSheet(savedInstanceState: Bundle?) {
-    bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_frame)
-    bottomSheetBehavior?.isHideable = true
-    bottomSheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
-    val scoreStarterController = ScoreStarterController(bottomSheetBehavior!!)
-    val scoreStarterPresenter = ScoreCreationPresenter(scoreStarterController)
-    main_starter_fab.setOnClickListener(scoreStarterController)
-    bottomSheetRouter = Conductor.attachRouter(this, bottom_sheet_frame, savedInstanceState)
-    bottomSheetRouter!!.setRoot(RouterTransaction.with(scoreStarterController))
-    scoreStarterPresenter.start()
-
-  }
 
   private fun setupToolbar() {
     setSupportActionBar(toolbar)
--- app/src/main/java/io/github/dmi3coder/scorsero/main/MainController.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/main/MainController.kt
@@ -1,35 +1,62 @@
 package io.github.dmi3coder.scorsero.main
 
+import android.os.Bundle
+import android.support.design.widget.BottomSheetBehavior
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bluelinelabs.conductor.Conductor
 import com.bluelinelabs.conductor.Controller
+import com.bluelinelabs.conductor.Router
+import com.bluelinelabs.conductor.RouterTransaction
 import io.github.dmi3coder.scorsero.R
 import io.github.dmi3coder.scorsero.data.Score
 import io.github.dmi3coder.scorsero.main.MainContract.Presenter
+import io.github.dmi3coder.scorsero.score.ScoreCreationPresenter
+import io.github.dmi3coder.scorsero.score.ScoreStarterController
 import io.reactivex.Flowable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
+import kotlinx.android.synthetic.main.controller_main.view.bottom_sheet_frame
 import kotlinx.android.synthetic.main.controller_main.view.main_list
+import kotlinx.android.synthetic.main.controller_main.view.main_starter_fab
 
 /**
  * Created by dim3coder on 6:49 PM 7/2/17.
  */
-class MainController : Controller(), MainContract.View {
+class MainController(val savedInstanceState: Bundle?) : Controller(), MainContract.View {
 
   var disposal: Disposable? = null
   internal lateinit var view: View
   internal lateinit var presenter: Presenter
   private var scoreAdapter: ScoreAdapter? = null
 
+  var bottomSheetRouter: Router? = null
+  var bottomSheetBehavior: BottomSheetBehavior<View>? = null
+
+
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup): View {
     view = inflater.inflate(R.layout.controller_main, container, false)
+    setupBottomSheet()
     return view
   }
 
   override fun setPresenter(presenter: Presenter) {
     this.presenter = presenter
+  }
+
+  fun setupBottomSheet() {
+    bottomSheetBehavior = BottomSheetBehavior.from(view.bottom_sheet_frame)
+    bottomSheetBehavior?.isHideable = true
+    bottomSheetBehavior?.state = BottomSheetBehavior.STATE_HIDDEN
+    val scoreStarterController = ScoreStarterController(bottomSheetBehavior!!)
+    val scoreStarterPresenter = ScoreCreationPresenter(scoreStarterController)
+    view.main_starter_fab.setOnClickListener(scoreStarterController)
+    bottomSheetRouter = Conductor.attachRouter(activity!!, view.bottom_sheet_frame,
+        savedInstanceState)
+    bottomSheetRouter!!.setRoot(RouterTransaction.with(scoreStarterController))
+    scoreStarterPresenter.start()
   }
 
   override fun showScores(scores: Flowable<List<Score>>) {

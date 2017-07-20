713a966ece8080675557e76e58a89000d0e94bc7
dmi3coder <dmi3coder@gmail.com>
2017-07-20 09:07:10 +0300

    ScoreCreation screen design raw

--- app/src/main/java/io/github/dmi3coder/scorsero/score/ScoreFieldAdapter.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/score/ScoreFieldAdapter.kt
@@ -5,23 +5,62 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.dmi3coder.scorsero.R
+import io.github.dmi3coder.scorsero.data.Score
+import kotlinx.android.synthetic.main.item_score_field.view.field_icon
+import kotlinx.android.synthetic.main.item_score_field.view.field_title
+import kotlinx.android.synthetic.main.item_score_field.view.field_value
+import org.joda.time.DateTime
 
 /**
  * Created by dim3coder on 8:23 AM 7/19/17.
  */
-class ScoreFieldAdapter(val fieldList: List<Triple<String,String,Any?>>?) : RecyclerView.Adapter<ScoreFieldHolder>() {
+class ScoreFieldAdapter(val score: Score) : RecyclerView.Adapter<ScoreFieldHolder>() {
 
   override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ScoreFieldHolder {
-    val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_score_field,parent,false)
+    val view = LayoutInflater.from(parent?.context).inflate(R.layout.item_score_field, parent,
+        false)
     return ScoreFieldHolder(view)
   }
 
+  /**
+   * Binds view to [score] fields, in a future this structure will be converted to map for
+   * modularity improvements (e.g extensible values)
+   */
   override fun onBindViewHolder(holder: ScoreFieldHolder?, position: Int) {
+    var title: String? = null
+    var value: String? = null
+    var drawable: Int? = null
+
+    when (position) {
+      0 -> {
+        title = "Title"
+        value = score.title
+        drawable = R.drawable.ic_chat
+      }
+      1 -> {
+        title = "Priority"
+        value = "${score.priority}st priority"
+        drawable = R.drawable.ic_notifications
+      }
+      2 -> {
+        title = "Date"
+        value = DateTime(score.creationDate).toString("dd MMM YYYY")
+        drawable = R.drawable.ic_event
+      }
+      3 -> {
+        title = "Description"
+        value = score.description
+        drawable = R.drawable.ic_chat
+      }
+    }
+    holder!!.itemView.apply {
+      this.field_title.text = title
+      this.field_value.text = value
+      this.field_icon.setImageResource(drawable!!)
+    }
   }
 
-  override fun getItemCount(): Int = 10
+  override fun getItemCount(): Int = 4
 }
 
-class ScoreFieldHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
-}
+class ScoreFieldHolder(itemView: View) : RecyclerView.ViewHolder(itemView)

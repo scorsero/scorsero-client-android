07a8e2d1a7778192b576e162b54ead4e7558ae62
dmi3coder <dmi3coder@gmail.com>
2017-08-04 09:09:15 +0300

    Drawer filling raw

--- app/src/main/java/io/github/dmi3coder/scorsero/navigation/DrawerAdapter.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/navigation/DrawerAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import io.github.dmi3coder.scorsero.R
+import kotlinx.android.synthetic.main.item_drawer_option.view.count
+import kotlinx.android.synthetic.main.item_drawer_option.view.title
 
 /**
  * Created by dim3coder on 9:06 AM 8/2/17.
@@ -18,10 +20,14 @@ class DrawerAdapter : RecyclerView.Adapter<DrawerItemHolder>() {
         LayoutInflater.from(parent!!.context).inflate(R.layout.item_drawer_option, parent, false))
   }
 
-  override fun getItemCount(): Int = 5
+  override fun getItemCount(): Int = 4
 
   override fun onBindViewHolder(holder: DrawerItemHolder?, position: Int) {
+    val view = holder!!.itemView
 
+    val options = view.context.resources.getStringArray(R.array.drawer_options)
+    view.title.text = options[position]
+    view.count.text = (Math.random() * 10).toInt().toString()
   }
 
 }

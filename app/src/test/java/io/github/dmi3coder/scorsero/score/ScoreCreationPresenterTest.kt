f5cdda011fce78a655f94172d6c0fcff7faf958e
dmi3coder <dmi3coder@gmail.com>
2017-09-10 20:26:30 +0300

    ScoreCreationPresenter insertion tests

--- app/src/test/java/io/github/dmi3coder/scorsero/score/ScoreCreationPresenterTest.kt
+++ app/src/test/java/io/github/dmi3coder/scorsero/score/ScoreCreationPresenterTest.kt
@@ -10,15 +10,24 @@ import android.view.KeyEvent
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Toolbar
+import com.nhaarman.mockito_kotlin.argumentCaptor
+import io.github.dmi3coder.scorsero.data.Score
 import io.github.dmi3coder.scorsero.data.source.ScoreRepository
+import io.github.dmi3coder.scorsero.main.time
+import io.github.dmi3coder.scorsero.score.ScoreCreationContract.ViewState.OPEN
+import org.hamcrest.CoreMatchers
 import org.hamcrest.CoreMatchers.equalTo
 import org.hamcrest.Matcher
+import org.hamcrest.Matchers
+import org.joda.time.DateTime
 import org.junit.After
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.ArgumentMatchers
+import org.mockito.ArgumentMatchers.*
 import org.mockito.InjectMocks
 import org.mockito.Mockito
 import org.mockito.Mockito.*
@@ -44,6 +53,30 @@ class ScoreCreationPresenterTest {
   @Test
   fun injectMocks_scoreCreationPresenterInjected() {
     assertThat(scoreCreationPresenter.repository, `is`(equalTo(scoreRepository)))
+  }
+
+  @Test
+  fun startScoreCreationPresenter_presenterSetToView() {
+    scoreCreationPresenter.start()
+    verify(scoreView).setPresenter(scoreCreationPresenter)
+  }
+
+  @Test
+  fun startScoreCreationPresenter_displayingTitle() {
+    val emptyScore = Score()
+    scoreCreationPresenter.start(emptyScore)
+    verify(scoreView).setScore(emptyScore)
+  }
+
+  @Test
+  fun processScore_scoreInsertedWithCreationDate() {
+    val testStartTime = DateTime().time
+    val scoreCaptor = argumentCaptor<Score>()
+    scoreCreationPresenter.processScore(Score(), OPEN)
+    verify(scoreRepository).insert(scoreCaptor.capture())
+    assertThat(scoreCaptor.lastValue.creationDate,
+        `is`(Matchers.greaterThanOrEqualTo(testStartTime)))
+    verify(scoreView).setScore(ArgumentMatchers.any())
   }
 
   @After fun tearDown() {}

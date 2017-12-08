c97105c5b16742bd567e8503be2bf8fa0702f4c1
Dmitry Chaban <dmitrychaban@yahoo.com>
2017-12-08 08:07:11 +0200

    Added simple line chart raw

--- app/src/main/java/io/github/dmi3coder/scorsero/navigation/DrawerController.kt
+++ app/src/main/java/io/github/dmi3coder/scorsero/navigation/DrawerController.kt
@@ -1,5 +1,6 @@
 package io.github.dmi3coder.scorsero.navigation
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.widget.LinearLayoutManager
@@ -8,6 +9,9 @@ import android.view.View
 import android.view.ViewGroup
 import com.bluelinelabs.conductor.Controller
 import com.bluelinelabs.conductor.Router
+import com.github.mikephil.charting.data.Entry
+import com.github.mikephil.charting.data.LineData
+import com.github.mikephil.charting.data.LineDataSet
 import io.github.dmi3coder.scorsero.BaseNavigator
 import io.github.dmi3coder.scorsero.BuildConfig
 import io.github.dmi3coder.scorsero.MainActivity
@@ -15,6 +19,13 @@ import io.github.dmi3coder.scorsero.R
 import io.github.dmi3coder.scorsero.navigation.NavigationContract.Presenter
 import kotlinx.android.synthetic.main.activity_main.drawer_layout
 import kotlinx.android.synthetic.main.controller_drawer.view.drawer_list
+import kotlinx.android.synthetic.main.controller_drawer.view.stats_chart
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.components.XAxis
+
+
+
+
 
 /**
  * Created by dim3coder on 8:53 AM 8/2/17.
@@ -36,7 +47,69 @@ class DrawerController() : Controller(), NavigationContract.View {
     drawer = (activity as MainActivity).drawer_layout //The hell I need this complexity?
     val presenter = NavigationPresenter(this, baseNavigator = activity as BaseNavigator)
     presenter.start()
+    showSummaryDiagram()
     return view!!
+  }
+
+  //TODO: connect repository to diagram
+  private fun showSummaryDiagram() {
+    var stats_chart = view!!.stats_chart
+    stats_chart.apply {
+      var dataSet = LineDataSet(
+              listOf(
+                      Entry(0f, 0f),
+                      Entry(1f, 2f),
+                      Entry(2f, 1f),
+                      Entry(3f, 2f),
+                      Entry(4f, 1f),
+                      Entry(5f, 2f),
+                      Entry(6f, 4f)
+              ),"test")
+      dataSet.color = Color.YELLOW
+      dataSet.setDrawCircles(true)
+      dataSet.setDrawValues(true)
+      dataSet.lineWidth = 2f
+      dataSet.mode = LineDataSet.Mode.CUBIC_BEZIER
+      this.data = LineData(dataSet)
+      this.invalidate()
+    }
+    val leftAxis = stats_chart.axisLeft
+    leftAxis.isEnabled = false
+    val rightAxis = stats_chart.axisRight
+    rightAxis.isEnabled = false
+    val xAxis = stats_chart.xAxis
+    xAxis.isEnabled = false
+    stats_chart.let {
+      it.isHorizontalScrollBarEnabled = false
+      it.isVerticalScrollBarEnabled= false
+      it.axisLeft.setDrawGridLines(false)
+      it.xAxis.setDrawGridLines(false)
+      it.setDrawBorders(false)
+      it.axisRight.setDrawGridLines(false)
+      it.setScaleEnabled(false)
+    }
+
+    drawer.addDrawerListener(object : DrawerLayout.DrawerListener{
+      override fun onDrawerSlide(drawerView: View?, slideOffset: Float) {
+
+      }
+
+      override fun onDrawerClosed(drawerView: View?) {
+        stats_chart.visibility = View.GONE
+      }
+
+      override fun onDrawerOpened(drawerView: View?) {
+        stats_chart.visibility = View.VISIBLE
+        stats_chart.animateXY(300,300)
+      }
+
+      override fun onDrawerStateChanged(newState: Int) {
+      }
+
+    })
+
+
+
   }
 
   override fun onRestoreViewState(view: View, savedViewState: Bundle) {
